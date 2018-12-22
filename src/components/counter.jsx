3dec01b3469224880ad5a47b68b644e8a996129e
iara99oliveira <iara99oliveira@gmail.com>
2018-12-22 20:57:58 -0200

    Implementado botão de deletar contador Modificado os valores das properties passados para o componente

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   render() {
@@ -11,6 +11,7 @@ class Counter extends Component {
         <div>
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
           <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Click Me!</button>
+          <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
       </React.Fragment>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,17 +3,33 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counter: [
+    counters: [
         {id: 1, value: 5},
         {id: 2, value: 0},
         {id: 3, value: 0},
         {id: 4, value: 0}
     ]
   };
+
+  handleDelete = (counterId) => {
+    console.log("Event handler called", counterId);
+    //instancia um novo array, não contendo o counter de id selecionado
+    const counters = this.state.counters.filter(counter => counter.id !== counterId);
+    // sobrescreve o array do state co o que acabamos de criar
+    this.setState({counters});
+
+  }
+
   render() {
     return (
       <div>
-        {this.state.counter.map((counter)=> <Counter key={counter.id} value={counter.value} />)}
+        {this.state.counters.map((counter)=> 
+            <Counter 
+                key={counter.id} 
+                onDelete={this.handleDelete} 
+                counter={counter} 
+            />
+        )}
       </div>
     );
   }
