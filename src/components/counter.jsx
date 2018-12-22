17185106a89a8695cd365cc51bfb7aa75cd19f81
iara99oliveira <iara99oliveira@gmail.com>
2018-12-22 16:25:35 -0200

    implementadas as funções de verificação de tags disponíveis e incrementar um número ao clicar o botão

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["this is a tag", "this is another tag", "and.. look!", "another one"]
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Click me :)</button>
+        <div>
+          <ul>{this.renderTags()}</ul>
+          {this.state.tags.length === 0 && "Please, insert a new tag."}
+        </div>
+        <span className="">{this.formatCount()}</span>
+        <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Click Me!</button>
       </React.Fragment>
     );
+  }
+  
+
+  handlerIncrement = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
+  getBadgeClasses(){
+
+  }
+
+  formatCount(){
+    let n = this.state.count;
+    return (n === 0)? "Zero": n;
   }
 }
 
