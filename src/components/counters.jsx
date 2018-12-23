6a8b45cc0c46df02bed085f91309ffc065c07942
iara99oliveira <iara99oliveira@gmail.com>
2018-12-23 17:33:28 -0200

    Implementado o botão de reset

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,43 +4,57 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-        {id: 1, value: 5},
-        {id: 2, value: 0},
-        {id: 3, value: 0},
-        {id: 4, value: 0}
+      { id: 1, value: 5 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
   };
 
   handleIncrement = counter => {
-
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
 
-    counters[index] = {...counter};
+    counters[index] = { ...counter };
     counters[index].value++;
-  
+
     this.setState({ counters: counters });
-  }
+  };
 
   handleDelete = counterId => {
-
     //instancia um novo array, não contendo o counter de id selecionado
-    const counters = this.state.counters.filter(counter => counter.id !== counterId);
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
     // sobrescreve o array do state com o que acabamos de criar
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
     this.setState({counters});
-  }
+  };
 
   render() {
     return (
       <div>
-        {this.state.counters.map((counter)=> 
-            <Counter 
-                key={counter.id} 
-                onIncrement={this.handleIncrement} 
-                onDelete={this.handleDelete} 
-                counter={counter} 
-            />
-        )}
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
       </div>
     );
   }
