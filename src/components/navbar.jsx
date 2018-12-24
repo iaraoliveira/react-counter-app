c73e8b7fc65fc727dfa86000ea2ffabcda67703b
iara99oliveira <iara99oliveira@gmail.com>
2018-12-24 15:49:25 -0200

    Stateless Functional Components - Criando um componente com uma função ao invés de uma classe

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <a href="" className="navbar-brand">
-                    My counters
-                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav> 
-        );
-    }
+// stateless functional component
+
+// visto que nosso componente não tem um state definido,
+// podemos criar uma função ao invés de uma classe ao criar o componente
+// o react vai passar as propriedades (props) por parâmetro
+
+// algum devs usam esse método, mas é opcional
+
+const Navbar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a href="" className="navbar-brand">
+                My counters
+                <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+            </a>
+        </nav> 
+     );
 }
  
-export default NavBar;
+export default Navbar;
+
+
+// classe antiga:
+
+// class NavBar extends Component {
+//     render() { 
+//         return ( 
+//             <nav className="navbar navbar-light bg-light">
+//                 <a href="" className="navbar-brand">
+//                     My counters
+//                     <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+//                 </a>
+//             </nav> 
+//         );
+//     }
+// }
+ 
+// export default NavBar;
